b6a44ea00ba7b536254d41c40d90693b9b2fda8c
Timm Knape <timm@kna-st.de>
2014-12-25 18:16:16 +0100

    Initial Commit

---
+++ Lispify/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Lispify
+//
+//  Created by Timm on 25.12.14.
+//  Copyright (c) 2014 Timm Knape Softwaretechnik. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
